7e1a4f543959788ad6fe1c5016e9b32952a8d4bd
Mark Sutherland <mark@marksuth.dev>
2021-10-07 21:10:38 +0100

    Add IndieWeb Webring

--- footer.php
+++ footer.php
@@ -15,6 +15,9 @@
 <footer class="site-footer py-4 text-center">
 	<div class="container">
 <p class="small">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<p><a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/previous">←</a>
+  An IndieWeb Webring 🕸💍
+  <a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/next">→</a></p>
 	</div>
 </footer>
 <?php wp_footer(); ?>
