155b75f43ca5479bd2a0740d3dfd7f976991605a
Mark Sutherland <mark@marksuth.dev>
2021-10-12 16:31:26 +0100

    fix header

--- header.php
+++ header.php
@@ -31,13 +31,14 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<nav class="navbar fixed-top navbar-expand-lg navbar-blur navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="/">MARK SUTHERLAND</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <nav class="navbar sticky-top navbar-expand-lg navbar-blur navbar-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">MARK SUTHERLAND</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavigation" aria-controls="mainNavigation" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="mainNavigation">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 	  <li class="nav-item">
           <a class="nav-link" href="/">Home</a>
@@ -47,10 +48,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/about/">About</a>
-        <!-- </li>
-		<li class="nav-item">
-          <a class="nav-link" href="/contact/">Contact</a>
-        </li> -->
+        </li>
       </ul>
     </div>
   </div>
