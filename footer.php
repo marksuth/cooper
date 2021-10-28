87818a5746310357c65677376b314e96b03241aa
Mark Sutherland <mark@marksuth.dev>
2021-10-28 07:59:09 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,13 +14,12 @@
 
 <footer class="site-footer py-4 mt-4 text-center bg-dark">
 	<div class="container">
-<p class="small">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-<p class="small">This site is carbon offset via <a href="https://treepoints.green/?ref=tTANfORx1u9H" rel="noopener">TreePoints</a> and hosted on <a href="https://m.do.co/c/0af74d4b6f61">Digital Ocean</a>.</p>
-  </div>
-<p><a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/previous">←</a>
+  <p><a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/previous">←</a>
   An IndieWeb Webring 🕸💍
   <a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/next">→</a></p>
-	</div>
+<p class="small">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<p class="small text-muted">This site is carbon offset via <a href="https://treepoints.green/?ref=tTANfORx1u9H" rel="noopener">TreePoints</a> and hosted on <a href="https://m.do.co/c/0af74d4b6f61">Digital Ocean</a>.</p>
+  </div>
 </footer>
 <?php wp_footer(); ?>
 
