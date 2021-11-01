ad3a5ac5d3ea3876d526162beffaa7df12684b63
Mark Sutherland <mark@marksuth.dev>
2021-11-01 00:39:09 +0000

    photo post formatting

--- single.php
+++ single.php
@@ -12,9 +12,36 @@ get_header();
 ?>
 <?php
 while (have_posts()) :
-	the_post(); ?>
+	the_post(); 
+	?>
 <div class="container">
 	<div class="row justify-content-center">
+	<?php if ( has_post_format('image') ) { ?>
+			<article class="col-md-6">
+				<div class="bg-white p-3 mb-3">
+				<main>
+					<?php the_content(); ?>
+				</main>
+				<header>
+						<h1 class="h5 pt-3 mb-0 text-dark"><?php the_title(); ?></h1>
+						<p class="text-muted"><?php the_date(); ?></p>
+				</header>
+				</div>
+				<footer>
+						<p class="text-muted">
+							<?php the_tags(); 
+							
+							wp_link_pages(
+						array(
+							'before' => '<div class="page-links">' . esc_html__('Pages:', 'cooper'),
+							'after'  => '</div>',
+						)
+					);
+					?>
+						</p>
+					</footer>
+				</article>
+		<?php } else { ?>
 		<div class="col-md-8">
 		<main id="primary" class="site-main">
 			<article>
@@ -40,8 +67,8 @@ while (have_posts()) :
 					</footer>
 				</article>
 			</main>
-<?php endwhile; ?>
 		</div>
+		<?php }; endwhile; ?>
 	</div>
 </div>
 
