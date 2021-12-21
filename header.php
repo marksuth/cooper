3a45bea4715b623928ed9d44305e438a51b64f4b
Mark Sutherland <mark@marksuth.dev>
2021-12-21 22:39:51 +0000

    Cleanup

--- header.php
+++ header.php
@@ -20,55 +20,42 @@
   <link rel="profile" href="https://gmpg.org/xfn/11">
 
   <?php wp_head(); ?>
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
-  <meta name="apple-mobile-web-app-title" content="Mark Sutherland">
-  <meta name="application-name" content="Mark Sutherland">
-  <meta name="msapplication-TileColor" content="#214154">
   <meta name="theme-color" content="#214154">
+  <script>
+    var _paq = window._paq = window._paq || [];
+    _paq.push(["trackPageView"]), _paq.push(["enableLinkTracking"]),
+      function() {
+        var a = "https://analytics.marksuth.dev/";
+        _paq.push(["setTrackerUrl", a + "matomo.php"]), _paq.push(["setSiteId", "1"]);
+        var e = document,
+          t = e.createElement("script"),
+          p = e.getElementsByTagName("script")[0];
+        t.async = !0, t.src = a + "matomo.js", p.parentNode.insertBefore(t, p)
+      }();
+  </script>
 </head>
-<script>
-  var _paq = window._paq = window._paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u = "https://analytics.marksuth.dev/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d = document,
-      g = d.createElement('script'),
-      s = d.getElementsByTagName('script')[0];
-    g.async = true;
-    g.src = u + 'matomo.js';
-    s.parentNode.insertBefore(g, s);
-  })();
-</script>
-
-</html>
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <nav class="navbar sticky-top navbar-expand-md navbar-blur navbar-dark">
     <div class="container">
-      <a class="navbar-brand" href="/">MARK SUTHERLAND</a>
+      <a class="navbar-brand" href="https://marksuth.dev">MARK SUTHERLAND</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavigation" aria-controls="mainNavigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="mainNavigation">
-      <?php
-            wp_nav_menu(array(
-                'theme_location' => 'main-menu',
-                'container' => false,
-                'menu_class' => '',
-                'fallback_cb' => '__return_false',
-                'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-lg-0 %2$s">%3$s</ul>',
-                'depth' => 2,
-                'walker' => new bs_nav_menu_walker()
-            ));
-            ?>
+        <?php
+        wp_nav_menu(array(
+          'theme_location' => 'main-menu',
+          'container' => false,
+          'menu_class' => '',
+          'fallback_cb' => '__return_false',
+          'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-lg-0 %2$s">%3$s</ul>',
+          'depth' => 2,
+          'walker' => new bs_nav_menu_walker()
+        ));
+        ?>
       </div>
     </div>
   </nav>
