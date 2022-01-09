d0af2cc4ebbef25fe6f81c4e4ab412dfa945afb1
Mark Sutherland <mark@marksuth.dev>
2022-01-09 01:43:26 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
 ?>
 <footer class="site-footer py-4 mt-4 text-center bg-dark">
   <div class="container">
-    <p><a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/previous">←</a>
-      An IndieWeb Webring 🕸💍
-      <a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/next">→</a>
+    <p><a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/previous"><i class="fa fa-chevron-left" title="Previous"></i></a>
+      An IndieWeb Webring
+      <a href="https://xn--sr8hvo.ws/%F0%9F%93%BB%F0%9F%93%85%F0%9F%9A%BF/next"><i class="fa fa-chevron-right" title="Next"></i></a>
     </p>
     <p class="small text-muted">&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. This site is carbon offset via <a href="https://treepoints.green/?ref=tTANfORx1u9H" rel="noopener"><i class="fa fa-tree"></i> TreePoints</a> and hosted on <a href="https://m.do.co/c/0af74d4b6f61"><i class="fab fa-digital-ocean"></i> Digital Ocean</a>.
       <br>Unless otherwise stated, this site is licensed under a <a href="https://creativecommons.org/licenses/by/3.0/" rel="noopener">Creative Commons Attribution 3.0 Unported License</a>.
