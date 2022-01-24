6795fae3a5e20ba26b75e4a710ae4ecc9ae309b7
Mark Sutherland <mark@marksuth.dev>
2022-01-24 08:04:32 +0000

    tidy up likes

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,11 @@ const mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 mix.js('resources/js/app.js', 'js')
     .sass('resources/sass/style.scss', '')
-    .purgeCss({
-        content: [('*.php')],
-        enabled: true,
-    })
+    // .purgeCss({
+    //     content: [('*.php')],
+    //     enabled: true,
+    //     whitelist: ['mention-list', 'hide-name', 'col-md-8', 'col-md-4'],
+    // })
     .options({
         autoprefixer: {
             options: {
